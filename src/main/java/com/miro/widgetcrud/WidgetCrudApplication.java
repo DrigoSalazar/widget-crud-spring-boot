4d7754ba61547d61d4e7ba573bca017bc1a90cd8
Rodrigo Salazar <drigoco@gmail.com>
2020-11-06 12:16:16 +0100

    Initial commit

---
+++ src/main/java/com/miro/widgetcrud/WidgetCrudApplication.java
@@ -0,0 +1,13 @@
+package com.miro.widgetcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miro/widgetcrud/WidgetCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.widgetcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
